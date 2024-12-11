762e0733c797a28442f3a8139265335e9645d0fe
Opjasson <azmitheusos333@gmail.com>
2024-12-11 18:00:56 +0700

    replace

--- AppCatatanPribadi/src/Components/NavbarBody.jsx
+++ AppCatatanPribadi/src/Components/NavbarBody.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import HeaderTitle from "../HeaderTitle";
-import "../../style/navbar.css";
-import FormControlled from "../ControlledComponent/FormControlled";
+import HeaderTitle from "./HeaderTitle.jsx";
+import "../style/navbar.css";
+import FormControlled from "./FormControlled.jsx";
 
-function NavbarBody({ classname }) {
+function NavbarBody() {
     return (
         <nav className="navbar">
             <HeaderTitle classname="h1">Notes</HeaderTitle>
--- AppCatatanPribadi/src/main.jsx
+++ AppCatatanPribadi/src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import NavbarBody from './Components/Navbar/NavbarBody.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <NavbarBody />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <AddCatatan />
+    </StrictMode>
+);
